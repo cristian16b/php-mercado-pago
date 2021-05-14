15d6a80764c5b1c14c69b81beca034ecbe6da846
cristian16b <cristian.budzicz@gmail.com>
2021-05-14 09:01:29 -0300

    se modifica la url de notificaciones

--- detail.php
+++ detail.php
@@ -108,8 +108,9 @@ $preference->items = array($item);
   );
 
 //   $preference->pa  payer($payer);
-$preference->notification_url = "https://cristian16b-mp-commerce-php.herokuapp.com/webhook.php";
-
+// $preference->notification_url = "https://cristian16b-mp-commerce-php.herokuapp.com/webhook.php";
+    
+    $preference->notification_url = "https://hookb.in/9XkBDBp1dDS600eMmdKo";
 
 $preference->back_urls = array(
     "success" => "https://cristian16b-mp-commerce-php.herokuapp.com/aprobado.php",
